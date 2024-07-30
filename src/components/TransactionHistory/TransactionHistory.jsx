cf6eeebb8740dd3dafd8120de1aae53e9d26c251
illja-popovics <143592959+illja-popovics@users.noreply.github.com>
2024-07-30 21:43:56 +0200

    added tasks

---
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -0,0 +1,42 @@
+import PropTypes from 'prop-types';
+import styles from './TransactionHistory.module.css';
+
+const capitalizeFirstLetter = (string) => {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+};
+
+const TransactionHistory = ({ items }) => {
+  return (
+    <table className={styles.transactionHistory}>
+      <thead>
+              <tr>
+          <th>Type</th>
+          <th>Amount</th>
+          <th>Currency</th>
+        </tr>
+      </thead>
+      <tbody>
+        {items.map(({ id, type, amount, currency }) => (
+          <tr key={id}>
+            <td>{capitalizeFirstLetter(type)}</td>
+            <td>{amount}</td>
+            <td>{currency}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+TransactionHistory.propTypes = {
+  items: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      type: PropTypes.string.isRequired,
+      amount: PropTypes.string.isRequired,
+      currency: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+};
+
+export default TransactionHistory;
